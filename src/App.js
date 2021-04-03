4d6ff9e2e296272857fa57122f5d3531d736dbce
xuanngo10 <xuan.ngo10@gmail.com>
2021-04-02 22:21:56 -0700

    progress

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import EnemyGrid from './components/EnemyGrid';
 import FriendlyGrid from './components/FriendlyGrid';
 import Header from './components/Header';
@@ -10,6 +10,201 @@ const App = () => {
   const [selected, setSelected] = useState('');
   const [direction, setDirection] = useState('Horizontal');
   const [disableOption, setDisableOption] = useState([]);
+  const [startGame, setStartGame] = useState(false);
+  const horEdge = [9, 19, 29, 39, 49, 59, 69, 79, 89, 99];
+  const horEdge2 = [
+    9,
+    19,
+    29,
+    39,
+    49,
+    59,
+    69,
+    79,
+    89,
+    99,
+    8,
+    18,
+    28,
+    38,
+    48,
+    58,
+    68,
+    78,
+    88,
+    98,
+  ];
+  const horEdge3 = [
+    9,
+    19,
+    29,
+    39,
+    49,
+    59,
+    69,
+    79,
+    89,
+    99,
+    8,
+    18,
+    28,
+    38,
+    48,
+    58,
+    68,
+    78,
+    88,
+    98,
+    7,
+    17,
+    27,
+    37,
+    47,
+    57,
+    67,
+    77,
+    87,
+    97,
+  ];
+  const horEdge4 = [
+    9,
+    19,
+    29,
+    39,
+    49,
+    59,
+    69,
+    79,
+    89,
+    99,
+    8,
+    18,
+    28,
+    38,
+    48,
+    58,
+    68,
+    78,
+    88,
+    98,
+    7,
+    17,
+    27,
+    37,
+    47,
+    57,
+    67,
+    77,
+    87,
+    97,
+    6,
+    16,
+    26,
+    36,
+    46,
+    56,
+    66,
+    76,
+    86,
+    96,
+  ];
+  const vertEdge = [90, 91, 92, 93, 94, 95, 96, 97, 98, 99];
+  const vertEdge2 = [
+    90,
+    91,
+    92,
+    93,
+    94,
+    95,
+    96,
+    97,
+    98,
+    99,
+    80,
+    81,
+    82,
+    83,
+    84,
+    85,
+    86,
+    87,
+    88,
+    89,
+  ];
+  const vertEdge3 = [
+    90,
+    91,
+    92,
+    93,
+    94,
+    95,
+    96,
+    97,
+    98,
+    99,
+    80,
+    81,
+    82,
+    83,
+    84,
+    85,
+    86,
+    87,
+    88,
+    89,
+    70,
+    71,
+    72,
+    73,
+    74,
+    75,
+    76,
+    77,
+    78,
+    79,
+  ];
+  const vertEdge4 = [
+    90,
+    91,
+    92,
+    93,
+    94,
+    95,
+    96,
+    97,
+    98,
+    99,
+    80,
+    81,
+    82,
+    83,
+    84,
+    85,
+    86,
+    87,
+    88,
+    89,
+    70,
+    71,
+    72,
+    73,
+    74,
+    75,
+    76,
+    77,
+    78,
+    79,
+    60,
+    61,
+    62,
+    63,
+    64,
+    65,
+    66,
+    67,
+    68,
+    69,
+  ];
 
   const Ship = (length, num) => {
     // example object {length: [0,1], hit: 1, sunk: true}
@@ -24,8 +219,16 @@ const App = () => {
 
   const generateEnemyShips = () => {
     // Generate random number
-    const randNum = max => {
-      return Math.floor(Math.random() * Math.floor(max));
+    // const randNum = max => {
+    //   return Math.floor(Math.random() * Math.floor(max));
+    // };
+    // something wrong with edgeArr
+    const randNum = (max, edgeArr) => {
+      const num = Math.floor(Math.random() * Math.floor(max));
+      console.log(edgeArr);
+      return horEdge.indexOf(num) > 0
+        ? Math.floor(Math.random() * Math.floor(max))
+        : num;
     };
     // Generate random direction
     const randomDir = () => {
@@ -37,14 +240,31 @@ const App = () => {
       const direction = randomDir();
       if (direction === 'horizontal') {
         let ships = [];
-        ships.push(randNum(90));
+        if (shipNum === 0) {
+          ships.push(randNum(90, horEdge));
+        } else if (shipNum === 1) {
+          ships.push(randNum(90, horEdge2));
+        } else if (shipNum === 2) {
+          ships.push(randNum(90, horEdge3));
+        } else if (shipNum === 3) {
+          ships.push(randNum(90, horEdge4));
+        }
+        // ships.push(randNum(90));
         for (let i = 0; i <= shipNum; i++) {
           ships.push(ships[ships.length - 1] + 1);
         }
         return ships;
       } else if (direction === 'vertical') {
         let ships = [];
-        ships.push(randNum(90));
+        if (shipNum === 0) {
+          ships.push(randNum(90, vertEdge));
+        } else if (shipNum === 1) {
+          ships.push(randNum(90, vertEdge2));
+        } else if (shipNum === 2) {
+          ships.push(randNum(90, vertEdge3));
+        } else if (shipNum === 3) {
+          ships.push(randNum(90, vertEdge4));
+        }
         for (let i = 0; i <= shipNum; i++) {
           ships.push(ships[ships.length - 1] + 10);
         }
@@ -68,10 +288,6 @@ const App = () => {
     };
 
     setEnemyShips(shipsArr());
-  };
-
-  const handleClick = () => {
-    generateEnemyShips();
   };
 
   const handleSelect = e => {
@@ -100,65 +316,84 @@ const App = () => {
     console.log();
   };
 
+  const start = () => {
+    setStartGame(true);
+    generateEnemyShips();
+  };
+
   const Gameboard = () => {};
 
-  // I need to figure this out
+  // Enemy ships cant overlap or go over edge
 
   return (
     <div className='App'>
       <Header />
       <div className='gameboard'>
         <div className='controller-container'>
-          <h3>Ships</h3>
-          <div>
-            <div
-              className={`select-ships ${
-                disableOption.indexOf('4') > -1 ? 'disable' : 'undefined'
-              }`}
-              data-num='4'
-              onClick={handleSelect}
-            >
-              Carrier
-            </div>
-            <div
-              className={`select-ships ${
-                disableOption.indexOf('3') > -1 ? 'disable' : 'undefined'
-              }`}
-              data-num='3'
-              onClick={handleSelect}
-            >
-              Battleship
-            </div>
-            <div
-              className={`select-ships ${
-                disableOption.indexOf('2') > -1 ? 'disable' : 'undefined'
-              }`}
-              data-num='2'
-              onClick={handleSelect}
-            >
-              Cruiser
-            </div>
-            <div
-              className={`select-ships ${
-                disableOption.indexOf('1') > -1 ? 'disable' : 'undefined'
-              }`}
-              data-num='1'
-              onClick={handleSelect}
-            >
-              Submarine
-            </div>
-            <div
-              className={`select-ships ${
-                disableOption.indexOf('0') > -1 ? 'disable' : 'undefined'
-              }`}
-              data-num='0'
-              onClick={handleSelect}
-            >
-              Destroyer
-            </div>
-          </div>
-          <button onClick={handleDirection}>{direction}</button>
-          <p>Restart</p>
+          {startGame === true ? (
+            <React.Fragment>
+              <h3>Stats</h3>
+              <div>
+                <p>0 successful hits</p>
+              </div>
+            </React.Fragment>
+          ) : disableOption.length > 4 ? (
+            <button onClick={start}>Start Game</button>
+          ) : (
+            <React.Fragment>
+              {' '}
+              <div>
+                <h3>Ships</h3>
+                <div
+                  className={`select-ships ${
+                    disableOption.indexOf('4') > -1 ? 'disable' : 'undefined'
+                  }`}
+                  data-num='4'
+                  onClick={handleSelect}
+                >
+                  Carrier
+                </div>
+                <div
+                  className={`select-ships ${
+                    disableOption.indexOf('3') > -1 ? 'disable' : 'undefined'
+                  }`}
+                  data-num='3'
+                  onClick={handleSelect}
+                >
+                  Battleship
+                </div>
+                <div
+                  className={`select-ships ${
+                    disableOption.indexOf('2') > -1 ? 'disable' : 'undefined'
+                  }`}
+                  data-num='2'
+                  onClick={handleSelect}
+                >
+                  Cruiser
+                </div>
+                <div
+                  className={`select-ships ${
+                    disableOption.indexOf('1') > -1 ? 'disable' : 'undefined'
+                  }`}
+                  data-num='1'
+                  onClick={handleSelect}
+                >
+                  Submarine
+                </div>
+                <div
+                  className={`select-ships ${
+                    disableOption.indexOf('0') > -1 ? 'disable' : 'undefined'
+                  }`}
+                  data-num='0'
+                  onClick={handleSelect}
+                >
+                  Destroyer
+                </div>
+              </div>
+              <button onClick={handleDirection}>{direction}</button>
+              <p>Restart</p>{' '}
+            </React.Fragment>
+          )}
         </div>
         <FriendlyGrid
           selected={selected}
@@ -167,9 +402,6 @@ const App = () => {
           resetSelect={resetSelect}
         />
         <EnemyGrid enemyFire={enemyFire} enemyShips={enemyShips} />
-      </div>
-      <div>
-        <button onClick={handleClick}>Start</button>
       </div>
     </div>
   );
